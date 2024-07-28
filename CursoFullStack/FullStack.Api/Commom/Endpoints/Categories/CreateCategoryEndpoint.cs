99af444bb0c7aa1f56cc656671f11367522b2588
Lucas Percisi <lucas_percisi@hotmail.com>
2024-07-28 13:14:49 -0300

    Segmentando os Endpoints

--- CursoFullStack/FullStack.Api/Commom/Endpoints/Categories/CreateCategoryEndpoint.cs
+++ CursoFullStack/FullStack.Api/Commom/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -1,12 +1,23 @@
 ﻿using FullStack.Api.Commom.Api;
+using FullStack.Core.Handlers;
+using FullStack.Core.Models;
+using FullStack.Core.Requests.Categories;
+using FullStack.Core.Responses;
 
 namespace FullStack.Api.Commom.Endpoints.Categories
 {
     public class CreateCategoryEndpoint : IEndpoint
     {
         public static void Map(IEndpointRouteBuilder app)
+            => app.MapPost("/", HandleAsync).Produces<Response<Category?>>();
+
+        private static async Task<IResult> HandleAsync(ICategoryHandler handler, CreateCategoryRequest request)
         {
-            throw new NotImplementedException();
+            var result = await handler.CreateAsync(request);
+            if (result.IsSucess)
+                Results.Created($"/{result.Data.Id}", result.Data);
+
+            return Results.BadRequest(result.Data);
         }
     }
 }

c80bbc8fb74f3b9a6de36e60177e542d91a37f4b
Lucas Percisi <lucas_percisi@hotmail.com>
2024-07-28 16:29:00 -0300

    Datetime extensions

--- CursoFullStack/FullStack.Core/Common/DateTimeExtesion.cs
+++ CursoFullStack/FullStack.Core/Common/DateTimeExtesion.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FullStack.Core.Common
+﻿namespace FullStack.Core.Common
 {
-    internal class DateTimeExtesion
+    public static class DateTimeExtesion
     {
+        public static DateTime GetFirstDay(this DateTime date, int? year = null, int? month = null)
+            => new(year ?? date.Year, month ?? date.Month, 1);
+
+        public static DateTime GetLastDay(this DateTime date, int? year = null, int? month = null)
+            => new DateTime(year ?? date.Year, month ?? date.Month, 1).AddMonths(1).AddDays(-1);
     }
 }

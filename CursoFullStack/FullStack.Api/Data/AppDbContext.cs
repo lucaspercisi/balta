8edd74c4c056553b5bc7067761ba38c880480986
Lucas Percisi <lucas_percisi@hotmail.com>
2024-08-11 13:13:31 -0300

    IdentityUser

--- CursoFullStack/FullStack.Api/Data/AppDbContext.cs
+++ CursoFullStack/FullStack.Api/Data/AppDbContext.cs
@@ -1,14 +1,14 @@
 ﻿using FullStack.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace FullStack.Api.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
     {
         public DbSet<Transaction> Transactions { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- CursoFullStack/FullStack.Api/Program.cs
+++ CursoFullStack/FullStack.Api/Program.cs
@@ -7,6 +7,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+builder.Services.AddAuthorization();
+
 var connectionString = builder
             .Configuration
             .GetConnectionString("DefaultConnection") ?? string.Empty;
@@ -16,18 +22,10 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(connectionString);
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
-builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
-builder.Services.AddAuthorization();
-
 var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.MapGet("/", () => new { message = "OK" });
 app.MapEndpoints();

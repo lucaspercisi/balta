9ac670fe3b00bcd353ab12c1f4fb774c9a14adfb
Lucas Percisi <lucas_percisi@hotmail.com>
2024-08-11 14:19:05 -0300

    Logout

--- CursoFullStack/FullStack.Api/Program.cs
+++ CursoFullStack/FullStack.Api/Program.cs
@@ -5,6 +5,7 @@ using FullStack.Api.Models;
 using FullStack.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,5 +47,37 @@ app.MapEndpoints();
 app.MapGroup("v1/identity")
     .WithTags("Identity")
     .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (SignInManager<User> signInManager) => 
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }).RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", (ClaimsPrincipal user) =>
+    {
+        if (user.Identity is null || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity
+            .FindAll(identity.RoleClaimType)
+            .Select(x => new 
+            {
+                x.Issuer,
+                x.OriginalIssuer,
+                x.Type,
+                x.Value,
+                x.ValueType
+            });
+
+        return TypedResults.Json(roles);
+
+    }).RequireAuthorization();
+
 
 app.Run();

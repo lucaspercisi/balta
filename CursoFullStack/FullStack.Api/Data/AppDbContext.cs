7fa11046e012a9958a41c004f59adbd8c320d965
Lucas Percisi <lucas_percisi@hotmail.com>
2024-08-11 13:20:18 -0300

    Identity DbContext

--- CursoFullStack/FullStack.Api/Data/AppDbContext.cs
+++ CursoFullStack/FullStack.Api/Data/AppDbContext.cs
@@ -1,11 +1,23 @@
-﻿using FullStack.Core.Models;
+﻿using FullStack.Api.Models;
+using FullStack.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace FullStack.Api.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options) 
+        : IdentityDbContext<
+            User, 
+            IdentityRole<long>,
+            long,
+            IdentityUserClaim<long>,
+            IdentityUserRole<long>,
+            IdentityUserLogin<long>,
+            IdentityRoleClaim<long>,
+            IdentityUserToken<long>>
+        (options)
     {
         public DbSet<Transaction> Transactions { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;

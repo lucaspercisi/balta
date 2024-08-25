6534ab0bfc1a2b6f48747fe9a627794e2277500c
Lucas Percisi <lucas_percisi@hotmail.com>
2024-08-25 16:26:00 -0300

    Adicionando suporte a light e dark mode

---
+++ CursoFullStack/FullStack.Web/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using MudBlazor;
+
+namespace FullStack.Web
+{
+    public static class Configuration
+    {
+        public static MudTheme Theme = new()
+        {
+            Typography = new Typography()
+            {
+                Default = new Default()
+                {
+                    FontFamily = ["Raleway", "sans-serif"]
+                }
+            },
+            Palette = new PaletteLight()
+            {
+                Primary = new MudBlazor.Utilities.MudColor("#1EFA2D"),
+                Secondary = Colors.LightGreen.Darken3,
+                Background = Colors.Grey.Lighten4,
+                AppbarBackground = new MudBlazor.Utilities.MudColor("#1EFA2D"),
+                AppbarText = Colors.Shades.Black,
+                TextPrimary = Colors.Shades.Black,
+                DrawerText = Colors.Shades.Black,
+                DrawerBackground = Colors.LightGreen.Lighten4,
+                PrimaryContrastText = Colors.Shades.Black
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = Colors.LightGreen.Accent3,
+                Secondary = Colors.LightGreen.Darken3,
+                AppbarBackground = Colors.LightGreen.Accent3,
+                AppbarText = Colors.Shades.Black
+            }
+        };
+    }
+}
